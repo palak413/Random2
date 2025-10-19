428c2c97591de51c45661bd762f3c2766c0eb0de
palak413 <palaksingh07005@gmail.com>
2025-10-19 22:14:17 +0530

    firstcommit

---
+++ 14loops/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to loops in golang")
+
+	days := []string{"Sunday", "Tuesday", "Wednesday", "Friday", "Saturday"}
+	fmt.Println(days)
+
+	// for d :=0 ; d< len(days); d++ {
+	// 	fmt.Println(days[d])
+
+	// }
+
+	for i := range days {
+		fmt.Println(days[i])
+	}
+
+}
